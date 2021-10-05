09dc29bcc8550843d0b52598ddbe5fb5ef301c85
Matthew <mjbme10@yahoo.com>
2021-10-05 17:17:42 -0500

    Added main.tf for app

---
+++ modules/app/main.tf
@@ -0,0 +1,49 @@
+resource "aws_iam_role" "dev-img-mgr-permissions-role" {
+  name = "test_role"
+
+  assume_role_policy = <<EOF
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "VisualEditor0",
+            "Effect": "Allow",
+            "Action": [
+                "s3:GetObject",
+                "s3:PutObject",
+                "s3:DeleteObject"
+            ],
+            "Resource": "arn:aws:s3:::img-mgr-t75675464/*"
+        },
+        {
+            "Sid": "VisualEditor1",
+            "Effect": "Allow",
+            "Action": [
+                "s3:ListBucket"
+            ],
+            "Resource": "arn:aws:s3:::img-mgr-t75675464"
+        },
+        {
+            "Sid": "VisualEditor2",
+            "Effect": "Allow",
+            "Action": [
+                "ec2:DescribeTags"
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+  EOF
+}
+
+
+module "s3-bucket" {
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "2.9.0"
+  # insert the 5 required variables here
+  acceleration_status = "Suspended"
+  bucket = "img-mgr-t75675464"
+  #policy = 
+}
+
+
