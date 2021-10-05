fa519d51f3e97d0f7e9b06c13c99a9cf938d4865
Matthew <mjbme10@yahoo.com>
2021-10-05 17:50:39 -0500

    Added variable.tf file

---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "namespace" {
+  description = "Name of img-mgr"
+  default = "img-mgr"
+  type = string
+}
+
+
+variable "region" {
+  description = "AWS Region"
+  default = "us-east-1"
+  type = string
+}
