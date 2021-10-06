9455b15f03fa5cc851e4c0dbdae81536f4519d37
Matthew <mjbme10@yahoo.com>
2021-10-06 11:02:43 -0500

    Added load balancer block in app module

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -110,3 +110,40 @@ resource "aws_launch_template" "img-mgr" {
 
   user_data = "${file("./modules/app/img-mgr.sh")}"
 }
+
+#-----Load Balancer-----
+resource "aws_elb" "img-mgr-lb" {
+  name               = "img-mgr-lb"
+  availability_zones = ["us-west-2a", "us-west-2b"]
+
+  #access_logs {
+  #  bucket        = "foo"
+  #  bucket_prefix = "bar"
+  #  interval      = 60
+  #}
+
+  listener {
+    instance_port     = 80
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+    target              = "HTTP:80/"
+    interval            = 30
+  }
+
+  instances                   = [aws_autoscaling_group.img-mgr-asg.id]
+  cross_zone_load_balancing   = true
+  idle_timeout                = 400
+  connection_draining         = true
+  connection_draining_timeout = 400
+
+  tags = {
+    Name = "img-mgr-lb"
+  }
+}
