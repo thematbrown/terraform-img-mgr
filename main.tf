cc08d355085a289dc7188c364df407053e0a3dd8
Matthew <mjbme10@yahoo.com>
2021-10-07 17:04:49 -0500

    Added definitions for vpc outputs

--- main.tf
+++ main.tf
@@ -4,10 +4,17 @@ terraform {
   }
 }
 
-module "VPC" {
+module "vpc" {
     source = "./modules/vpc"    
 }
 
-#module "app" {
-#  source = "./modules/app-asg"
-#}
+module "app-asg" {
+  source          = "./modules/app-asg"
+
+  vpc_id          = module.vpc.vpc_id
+  vpc             = module.vpc
+  vpc_cidr        = module.vpc.vpc_cidr
+  private_subnets = module.vpc.private_subnets
+  public_subnets  = module.vpc.public_subnets
+  #azs             = module.vpc.azs
+}
