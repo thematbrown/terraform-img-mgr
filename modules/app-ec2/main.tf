1eb377fadbea13c579c21fee9ce03d4774724561
Matthew <mjbme10@yahoo.com>
2021-10-06 20:47:49 -0500

    Added app-ec2 module for single ec2 instance

---
+++ modules/app-ec2/main.tf
@@ -0,0 +1,61 @@
+#-----Load Balancer-----
+resource "aws_elb" "img-mgr-lb" {
+  name               = "img-mgr-lb"
+  availability_zones = ["us-west-2a", "us-west-2b"]
+
+  #access_logs {
+  #  bucket        = "foo"
+  #  bucket_prefix = "bar"
+  #  interval      = 60
+  #}
+
+  listener {
+    instance_port     = 80
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+    target              = "HTTP:80/"
+    interval            = 30
+  }
+
+  instances                   = [aws_autoscaling_group.img-mgr-asg.id]
+  cross_zone_load_balancing   = true
+  idle_timeout                = 400
+  connection_draining         = true
+  connection_draining_timeout = 400
+
+  tags = {
+    Name = "img-mgr-lb"
+  }
+}
+
+data "aws_ami" "amazon" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "img-mgr"
+  }
+}
